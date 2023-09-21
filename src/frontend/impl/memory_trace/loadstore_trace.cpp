58f28191de17ccbc2453a47df2c47636a07e8edd
Haocong Luo <haocong.luo@inf.ethz.ch>
2023-09-21 19:22:20 +0200

    Fix bug in LDST trace frontend (Issue #10)

--- src/frontend/impl/memory_trace/loadstore_trace.cpp
+++ src/frontend/impl/memory_trace/loadstore_trace.cpp
@@ -40,7 +40,7 @@ class LoadStoreTrace : public IFrontEnd, public Implementation {
 
     void tick() override {
       const Trace& t = m_trace[m_curr_trace_idx];
-      bool request_sent = m_memory_system->send({t.addr, t.is_write ? Request::Type::Read : Request::Type::Write});
+      bool request_sent = m_memory_system->send({t.addr, t.is_write ? Request::Type::Write : Request::Type::Read});
       if (request_sent) {
         m_curr_trace_idx = (m_curr_trace_idx + 1) % m_trace_length;
         m_trace_count++;
