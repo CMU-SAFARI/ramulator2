7ff037ddd4f57314ec6ebd9266518ec500d4e180
Haocong Luo <haocong.luo@safari.ethz.ch>
2026-04-14 05:56:29 +0000

    Test harness reorganization

--- tests/smoke/utils.py
+++
@@ -1,5 +0,0 @@
-"""Smoke-suite wrappers around shared simulation runners."""
-
-from tests.utils.sim import run_single
-
-__all__ = ["run_single"]
--- tests/utils/dram.py
+++
@@ -1,9 +0,0 @@
-"""Shared DRAM creation helper for test suites."""
-
-import ramulator
-
-
-def create_dram(cfg):
-    """Instantiate a DRAM object from a testcase CONFIG dict."""
-    dram_cls = getattr(ramulator.dram, cfg["dram_class"])
-    return dram_cls(org_preset=cfg["org_preset"], timing_preset=cfg["timing_preset"], **cfg["dram_kwargs"])
